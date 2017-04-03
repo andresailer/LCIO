681b0fb588d27a488971052e529bce3761060574
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-04-03 16:08:08 +0200

    Travis: Install java

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -3,11 +3,15 @@
 ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
 source $ILCSOFT/init_ilcsoft.sh
 
+
+ls -l /cvmfs/sft.cern.ch/lcg/releases/LCG_88/java/8u91/x86_64-slc6-gcc62-opt/
+
 cd /Package
 mkdir build
 cd build
 cmake -GNinja -DUSE_CXX11=ON -DBUILD_ROOTDICT=ON -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" \
       -D LCIO_GENERATE_HEADERS=ON \
+      -D JAVA_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_88/java/8u91/x86_64-slc6-gcc62-opt/ \
       -D INSTALL_JAR=ON -D LCIO_JAVA_USE_MAVEN=OFF .. && \
 ninja && \
 ninja install && \
